7aaa375edd8b4f06a8d0af1ba5efbe796d6a06c5
aureliennezzar <a.nezzar@subskill.com>
2023-11-08 22:32:13 +0100

    fix: single component route name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'single/:id', component: SingleComponent },
+  { path: 'character/:id', component: SingleComponent },
   {
     path: '**',
     redirectTo: ''
