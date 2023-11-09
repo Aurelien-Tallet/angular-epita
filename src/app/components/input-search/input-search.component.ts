0f4f87eb17b1f654a34b4135ae51c178234eba3f
Aurelien-Tallet <aurelien.jarte@gmail.com>
2023-11-09 20:53:38 +0100

    WIP

--- src/app/components/input-search/input-search.component.ts
+++ src/app/components/input-search/input-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-input-search',
