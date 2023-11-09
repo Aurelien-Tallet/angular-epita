2f6c3e783ccf38e311304248b5ff8fa878314fac
Aurelien-Tallet <aurelien.jarte@gmail.com>
2023-11-09 20:53:38 +0100

    WIP

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Component, Input } from '@angular/core';
+import { MarvelService } from '@services/marvel.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  characters: Array<any> = [];
+  constructor(private marvelService: MarvelService) {
+  }
+  ngOnInit(): void {
+    this.marvelService.getAllCharacters().subscribe(({ data }) => {
+      this.characters = data.results;
+    });
+  }
 
 }
