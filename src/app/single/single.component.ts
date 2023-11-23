b3d579bfd5f71579b1374fbdd686093f3b611196
aureliennezzar <a.nezzar@subskill.com>
2023-11-23 14:26:18 +0100

    style added

--- src/app/single/single.component.ts
+++ src/app/single/single.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ApiService } from "@services/api.service";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ApiService} from "@services/api.service";
 
 @Component({
   selector: 'app-single',
   templateUrl: './single.component.html',
-  styleUrls: ['./single.component.scss']
+  styleUrls: ['./single.component.scss'],
 })
 export class SingleComponent implements OnInit {
   public cocktail: any;
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {
+  }
+
   getIngredients(cocktail: any): string[] {
     const ingredients = [];
     for (let i = 1; i <= 15; i++) {
-      if (cocktail[`strIngredient${i}`]) {
-        ingredients.push(`${cocktail[`strIngredient${i}`]} - ${cocktail[`strMeasure${i}`] || ''}`);
+      try {
+        if (cocktail[`strIngredient${i}`]) {
+          ingredients.push(`${cocktail[`strIngredient${i}`]} - ${cocktail[`strMeasure${i}`] || ''}`);
+        }
+      } catch (e) {
+        break;
       }
     }
     return ingredients;
   }
+
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       const charId = params["id"];
-      this.apiService.getSingle(charId).subscribe(({ drinks }) => {
+      this.apiService.getSingle(charId).subscribe(({drinks}) => {
         this.cocktail = drinks[0];
       });
     });
