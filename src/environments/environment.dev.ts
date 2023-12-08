1bb3b741e53f807de1b0041c3a30aeb5415e6a48
aureliennezzar <a.nezzar@subskill.com>
2023-12-08 17:03:24 +0100

    feat: endpoint of strak-api added

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,6 +3,6 @@ export default {
   baseUrl: 'https://the-cocktail-db.p.rapidapi.com',
   apiKey: 'dba7e85a68msh851bc2ebc2f8870p12af62jsn911ec0ba365c',
   apiHost: 'the-cocktail-db.p.rapidapi.com',
-  categoriesEndpoint: ''
+  categoriesEndpoint: 'http://api.aurelien-nezzar.com/api/angular/getCategories',
 } as const;
 
